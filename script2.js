11e53208db6173019688c60fd1d27dbaedff3fd1
Corse2B <tsilvain@lfitokyo.org>
2025-12-17 08:39:08 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,26 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // Sélection des éléments
   const photos = document.querySelectorAll(".photo");
   const overlay = document.getElementById("overlay");
-  const page = document.getElementById("page");
 
   function openPhoto(photo) {
     photo.classList.add("active");
+    overlay.style.display = "block";
     document.body.classList.add("no-scroll");
   }
 
   function closeAll() {
-    photos.forEach(photo => {
-      photo.classList.remove("active");
-    });
+    photos.forEach(photo => photo.classList.remove("active"));
+    overlay.style.display = "none";
     document.body.classList.remove("no-scroll");
   }
 
   photos.forEach(photo => {
     photo.addEventListener("click", (e) => {
       e.stopPropagation();
-      openPhoto(photo);
+
+      if (photo.classList.contains("active")) {
+        closeAll();
+      } else {
+        closeAll();
+        openPhoto(photo);
+      }
     });
   });
 
