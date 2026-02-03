9549199de5fe93f222915fef78930ebad4f63838
Thibault <tsilvain@lfitokyo.org>
2026-02-03 11:44:46 +0900

    Create script8.js

---
+++ script8.js
@@ -0,0 +1,34 @@
+// Hash SHA-256 du mot de passe ("hello")
+const bonHash =
+  "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
+
+// Vérifie si déjà connecté
+if (localStorage.getItem("ok") === "true") {
+  afficherSite();
+}
+
+async function verifier() {
+  const input = document.getElementById("mdp").value;
+
+  const hashBuffer = await crypto.subtle.digest(
+    "SHA-256",
+    new TextEncoder().encode(input)
+  );
+
+  const hashHex = [...new Uint8Array(hashBuffer)]
+    .map(b => b.toString(16).padStart(2, "0"))
+    .join("");
+
+  if (hashHex === bonHash) {
+    localStorage.setItem("ok", "true");
+    afficherSite();
+  } else {
+    document.getElementById("erreur").textContent = "Mot incorrect";
+  }
+}
+
+function afficherSite() {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("site").style.display = "block";
+}
+
