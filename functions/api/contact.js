85cdd0c3e791c5e61c1bc4751fb5eafc1120d8ea
Thibault <tsilvain@lfitokyo.org>
2026-02-06 20:15:36 +0900

    Update contact.js

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -52,7 +52,7 @@ export const onRequestPost = async ({ request, env }) => {
           }
         ],
         from: {
-          email: "no-reply@chronographia5dsia.pages.dev",
+          email: "no-reply@chronographia5dsia.pages",
           name: "Chronographia 5DSIA"
         },
         subject: "Formulaire Chronographia",
