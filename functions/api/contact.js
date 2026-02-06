318a8f9def9520c8524b671b588e9890d91609cf
Thibault <tsilvain@lfitokyo.org>
2026-02-06 19:19:15 +0900

    Update contact.js

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -1,30 +1,40 @@
 export const onRequestPost = async ({ request, env }) => {
-  const formData = await request.formData();
+  try {
+    const formData = await request.formData();
 
-  const token = formData.get("cf-turnstile-response");
-  if (!token) {
-    return new Response("Turnstile manquant", { status: 400 });
-  }
-
-  const verify = await fetch(
-    "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({
-        secret: env.TURNSTILE_SECRET,
-        response: token,
-      }),
+    const token = formData.get("cf-turnstile-response");
+    if (!token) {
+      return new Response("Turnstile manquant", { status: 400 });
     }
-  );
 
-  const result = await verify.json();
+    if (!env.TURNSTILE_SECRET) {
+      return new Response("Secret Turnstile manquant côté serveur", {
+        status: 500,
+      });
+    }
 
-  if (!result.success) {
-    return new Response("Échecs Turnstile", { status: 403 });
+    const verify = await fetch(
+      "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          secret: env.TURNSTILE_SECRET,
+          response: token,
+        }),
+      }
+    );
+
+    const result = await verify.json();
+
+    if (!result.success) {
+      return new Response("Échec Turnstile", { status: 403 });
+    }
+
+    return Response.redirect("/merci", 302);
+  } catch (err) {
+    return new Response("Erreur serveur", { status: 500 });
   }
-
-  return Response.redirect("/merci", 302);
 };
