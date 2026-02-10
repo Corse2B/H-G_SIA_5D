c4d432ae5af952bccd25225294869e8cf7cae548
Thibault <tsilvain@lfitokyo.org>
2026-02-10 12:25:21 +0900

    Update certificat.js

--- public/js/certificat.js
+++ public/js/certificat.js
@@ -1,35 +1,16 @@
-const API_POST = "/api/certificat";
 const CERTIFICAT_IMAGE = "/certificat.jpg";
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const inputNom = document.getElementById("nom");
 const btn = document.getElementById("btnCertificat");
-const zoneResultat = document.getElementById("resultat");
 
-async function genererCertificat() {
+function genererCertificat() {
   const nom = inputNom.value.trim();
-
-  if (!nom || nom.length > 50) {
-    alert("Nom invalide");
+  if (!nom) {
+    alert("Nom manquant");
     return;
   }
-
-  const response = await fetch(API_POST, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      nom,
-      mission: "Mission Chateaux Forts"
-    })
-  });
-
-  if (!response.ok) {
-    alert("Erreur API");
-    return;
-  }
-
-  const { id } = await response.json();
 
   const image = new Image();
   image.src = CERTIFICAT_IMAGE;
@@ -38,25 +19,23 @@ async function genererCertificat() {
     canvas.width = image.width;
     canvas.height = image.height;
 
-    ctx.drawImage(image, 0, 30);
-    ctx.font = "65px Gothic";
+    ctx.drawImage(image, 0, 0);
+
+    ctx.font = "60px serif";
+    ctx.fillStyle = "gold";
     ctx.textAlign = "center";
-    ctx.fillStyle = "#e6ae47";
     ctx.fillText(nom, canvas.width / 2, 380);
 
-    ctx.font = "20px monospace";
-    ctx.fillText("ID : " + id, canvas.width / 2, 430);
+    const nomFichier = nom
+      .toLowerCase()
+      .replace(/[^a-z0-9]/g, "_");
 
-    const a = document.createElement("a");
-    a.download = `certificat_${id}.jpg`;
-    a.href = canvas.toDataURL("image/jpeg");
-    a.click();
-
-    zoneResultat.innerHTML = `
-      ✅ Certificat généré<br>
-      ID : <b>${id}</b>
-    `;
+    const lien = document.createElement("a");
+    lien.download = `certificat_${nomFichier}.jpg`;
+    lien.href = canvas.toDataURL("image/jpeg");
+    lien.click();
   };
 }
 
 btn.addEventListener("click", genererCertificat);
+
