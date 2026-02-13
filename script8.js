6fad5e2fdae2200a35b58847e4d7611650235f0a
Thibault <tsilvain@lfitokyo.org>
2026-02-13 12:27:59 +0900

    Update script8.js

--- script8.js
+++ script8.js
@@ -1,40 +1,17 @@
-// =======================
-// CONFIGURATION
-// =======================
-
-// SALT (différent du mot de passe)
-const SALT = "chronographia5dsia3245684902";
-
-
-const bonHash =
-  "bc7835aab1efed317a6fc2732f948d82d4ca23644bff0eb8c73d1f16fbcc8510";
-
-// =======================
-// AUTO-LOGIN
-// =======================
-if (localStorage.getItem("ok") === "true") {
-  afficherSite();
-}
-
-// =======================
-// VÉRIFICATION
-// =======================
 async function verifier() {
-  const input = document.getElementById("mdp").value;
+  const password = document.getElementById("mdp").value;
 
-  // ⚠️ IMPORTANT : séparateur ESPACE
-  const data = `${input} ${SALT}`;
+  const fd = new FormData();
+  fd.append("password", password);
 
-  const hashBuffer = await crypto.subtle.digest(
-    "SHA-256",
-    new TextEncoder().encode(data)
-  );
+  const response = await fetch("/api/login", {
+    method: "POST",
+    body: fd
+  });
 
-  const hashHex = [...new Uint8Array(hashBuffer)]
-    .map(b => b.toString(16).padStart(2, "0"))
-    .join("");
+  const data = await response.json();
 
-  if (hashHex === bonHash) {
+  if (data.success) {
     localStorage.setItem("ok", "true");
     afficherSite();
   } else {
@@ -42,31 +19,3 @@ async function verifier() {
   }
 }
 
-// =======================
-// AFFICHAGE
-// =======================
-function afficherSite() {
-  document.getElementById("login").style.display = "none";
-  document.getElementById("site").style.display = "block";
-}
-
-// =======================
-// ANTI-FLASH
-// =======================
-document.addEventListener("DOMContentLoaded", () => {
-  document.documentElement.style.visibility = "visible";
-});
-// =======================
-// ENTER = vérifier
-// =======================
-document.addEventListener("DOMContentLoaded", () => {
-  const inputMdp = document.getElementById("mdp");
-
-  inputMdp.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      verifier();
-    }
-  
-  });
-});
